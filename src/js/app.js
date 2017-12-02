12344ae09a2f62af965fa3d10ea30719be133355
Lorena Calaça <lorena.calaca.od@gmail.com>
2017-12-02 21:51:36 -0200

    feat: updated app.js to use jquery

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,40 @@
-// Color picker
-    const linePicker = document.getElementById('line-color');
-    const lineWrapper = document.getElementById('line-color-wrapper');
-    const bgPicker = document.getElementById('bg-color');
-    const bgWrapper = document.getElementById('bg-color-wrapper');
+$(document).ready(function() {
+    colorPicker();
+});
 
-    linePicker.onchange = function () {
-        lineWrapper.style.backgroundColor = linePicker.value;
-    }
+/**
+* @description Updates colors when they change
+*/
+function colorPicker() {
+    const linePicker  = $('#line-color');
+    const lineWrapper = $('#line-color-wrapper');
+    const bgPicker    = $('#bg-color');
+    const bgWrapper   = $('#bg-color-wrapper');
+    const canvas      = $('#canvas');
 
-    lineWrapper.style.backgroundColor = linePicker.value;
+    colorPickerHandler(canvas, linePicker, lineWrapper, bgPicker, bgWrapper);
+};
 
-    bgPicker.onchange = function () {
-        bgWrapper.style.backgroundColor = bgPicker.value;
-    }
+/**
+* @description Handles color changes
+* @param {object} canvas - The canvas/table to be painted on
+* @param {object} linePicker - The line color picker
+* @param {object} lineWrapper - The line color picker wrapper
+* @param {object} bgPicker - The background color picker
+* @param {object} bgWrapper - The background color picker wrapper
+*/
+function colorPickerHandler(canvas, linePicker, lineWrapper, bgPicker, bgWrapper) {
+    linePicker.change(function () {
+        lineWrapper.css('backgroundColor', linePicker.val());
+    });
 
-    bgWrapper.style.backgroundColor = bgPicker.value;
+    lineWrapper.css('backgroundColor', linePicker.val());
+
+    bgPicker.change(function () {
+        canvas.css('backgroundColor', bgPicker.val());
+        bgWrapper.css('backgroundColor', bgPicker.val());
+    });
+
+    canvas.css('backgroundColor', bgPicker.val());
+    bgWrapper.css('backgroundColor', bgPicker.val());
+};
